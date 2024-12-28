97767002f1d10105068f9d170ad8e97736564c74
Diana Avedisyan <diana-avedisyan@mail.ru>
2024-12-28 13:49:23 +0500

    Update App.jsx and ShopItemFunc.jsx

--- src/components/ShopItemFunc.jsx
+++ src/components/ShopItemFunc.jsx
@@ -1,33 +1,22 @@
-import Item from '../classes/Item';
+const ShopItemFunc = (item) => {
+  const { brand, title, description, descriptionFull, currency, price } = item;
 
-const ShopItemFunc = () => {
-  const item = new Item(
-    'Tiger of Sweden',
-    'Leonard coat',
-    'Minimalistic coat in cotton-blend',
-    'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-    399,
-    '£'
-  );
-
-  if (item instanceof Item) {
-    return (
-      <div className="main-content">
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
-        <div className="description">
-          {item.descriptionFull}
-        </div>
-        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
-        <div className="divider"></div>
-        <div className="purchase-info">
-          <div className="price">{item.currency}{item.price.toFixed(2)}</div>
-          <button>Добавить в корзину</button>
-        </div>
+  return (
+    <div className="main-content">
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">
+        {descriptionFull}
       </div>
-    );
-  }
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{currency}{Number(price).toFixed(2)}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
 };
 
 export default ShopItemFunc;
